ef1a602743ba308db8b5af2b6048a20c45376350
MarmureanuVlad <vlad.marmureanu@gmail.com>
2024-03-09 16:53:15 +0200

    Third commit

---
+++ Sem3.c
@@ -0,0 +1,89 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+struct Aeroport {
+    int id;
+    char* nume;
+    int nrTerminale;
+};
+
+struct Aeroport* adaugaAeroport(struct Aeroport* vector, struct Aeroport aeroport, int* dimensiune) {
+    struct Aeroport* copie = (struct Aeroport*)malloc(sizeof(struct Aeroport) * ((*dimensiune) + 1));
+    copie[(*dimensiune)] = aeroport;
+    for (int i = 0; i < (*dimensiune); i++) {
+        copie[i] = vector[i];
+    }
+    if (vector != NULL) {
+        free(vector);
+    }
+    (*dimensiune)++;
+    return copie;
+}
+
+struct Aeroport* citireFisier(const char* numeFisier, int* nrAeroporturi) {
+    struct Aeroport* vectorAeroporturi = NULL;
+    FILE* f = fopen(numeFisier, "r+");
+    if (f != NULL) {
+        (*nrAeroporturi) = 0;
+        char buffer[100];
+        while (fgets(buffer, 99, f) != NULL) {
+            struct Aeroport aeroport;
+            char delimitator[] = ",\n";
+            char* token = strtok(buffer, delimitator);
+            aeroport.id = atoi(token);
+            token = strtok(NULL, delimitator);
+            aeroport.nume = malloc(sizeof(char) * (strlen(token) + 1));
+            strcpy(aeroport.nume, token);
+            token = strtok(NULL, delimitator);
+            aeroport.nrTerminale = atoi(token);
+
+            vectorAeroporturi = adaugaAeroport(vectorAeroporturi, aeroport, nrAeroporturi);
+        }
+        fclose(f);
+    }
+    return vectorAeroporturi;
+}
+
+void afisareAeroport(struct Aeroport* aeroport) {
+    printf("Aeroportul cu ID-ul : %d cu numele : %s are %d terminale\n", aeroport->id, aeroport->nume, aeroport->nrTerminale);
+}
+
+void afisareVectorAeroporturi(struct Aeroport* aeroporturi, int dim) {
+    printf("Afisare Vector: \n");
+    for (int i = 0; i < dim; i++) {
+        afisareAeroport(&aeroporturi[i]);
+    }
+}
+
+void DezalocareAeroporturi(struct Aeroport** aeroporturi, int* dim) {
+    for (int i = 0; i < *dim; i++) {
+        free((*aeroporturi)[i].nume);
+    }
+    free(*aeroporturi);
+    (*aeroporturi) = NULL;
+    (*dim) = 0;
+}
+
+struct Aeroport idCautat(struct Aeroport* aeroporturi, int dim, int idCautat) {
+    for (int i = 0; i < dim; i++) {
+        if (aeroporturi[i].id == idCautat) {
+            return aeroporturi[i];
+        }
+    }
+    struct Aeroport aeroport;
+    aeroport.id = -1;
+    aeroport.nrTerminale = -1;
+    aeroport.nume = "N/A";
+    return aeroport;
+}
+
+int main() {
+    int dimensiune = 0;
+    struct Aeroport* aeroporturi = NULL;
+    aeroporturi = citireFisier("aeroporturi.txt", &dimensiune);
+    afisareVectorAeroporturi(aeroporturi, dimensiune);
+    DezalocareAeroporturi(&aeroporturi, &dimensiune);
+    return 0;
+}
